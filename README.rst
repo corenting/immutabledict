ec94e0a8fa01d4513a6d173cb618465f1bb823fb
Corentin Garcia <cgarcia@meilleursagents.com>
2020-01-08 13:49:35 +0100

    Initial commit

---
+++ README.rst
@@ -0,0 +1,7 @@
+==========
+immutabledict
+==========
+
+A fork of ``frozendict``, an immutable wrapper around dictionaries.
+
+See the original project here: https://github.com/slezica/python-frozendict
---
+++ immutabledict/__init__.py
@@ -0,0 +1,55 @@
+import collections
+import operator
+import functools
+import sys
+from collections import OrderedDict
+
+
+__version__ = '0.1.0'
+
+
+class immutabledict(collections.Mapping):
+    """
+    An immutable wrapper around dictionaries that implements the complete :py:class:`collections.Mapping`
+    interface. It can be used as a drop-in replacement for dictionaries where immutability is desired.
+    """
+
+    dict_cls = dict
+
+    def __init__(self, *args, **kwargs):
+        self._dict = self.dict_cls(*args, **kwargs)
+        self._hash = None
+
+    def __getitem__(self, key):
+        return self._dict[key]
+
+    def __contains__(self, key):
+        return key in self._dict
+
+    def copy(self, **add_or_replace):
+        return self.__class__(self, **add_or_replace)
+
+    def __iter__(self):
+        return iter(self._dict)
+
+    def __len__(self):
+        return len(self._dict)
+
+    def __repr__(self):
+        return '<%s %r>' % (self.__class__.__name__, self._dict)
+
+    def __hash__(self):
+        if self._hash is None:
+            h = 0
+            for key, value in self.items():
+                h ^= hash((key, value))
+            self._hash = h
+        return self._hash
+
+
+class ImmutableOrderedDict(immutabledict):
+    """
+    A frozendict subclass that maintains key order
+    """
+
+    dict_cls = OrderedDict
