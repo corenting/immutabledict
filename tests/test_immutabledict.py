3930e6919c55146ec63993cf01a5478c9200f951
Corentin Garcia <cgarcia@meilleursagents.com>
2020-01-09 13:47:46 +0100

    Started adding tests

--- tests/test_immutabledict.py
+++ tests/test_immutabledict.py
@@ -1,5 +1,34 @@
-from immutabledict import __version__
+import pytest
+
+from immutabledict import ImmutableOrderedDict, immutabledict
 
 
-def test_version():
-    assert __version__ == '0.1.0'
+class TestImmutableDict:
+    def test_cannot_assign_value(self):
+        with pytest.raises(AttributeError):
+            immutabledict().setitem('key', 'value')
+
+    def test_init_and_compare(self):
+        normal_dict = {
+            'a': 'value',
+            'b': 'other_value'
+        }
+        immutable_dict = immutabledict(normal_dict)
+        assert immutable_dict == normal_dict
+
+    def test_get_existing(self):
+        immutable_dict = immutabledict({ 'a': 'value' })
+        assert immutable_dict['a'] == 'value'
+
+    def test_get_not_existing(self):
+        immutable_dict = immutabledict({ 'a': 'value' })
+        with pytest.raises(KeyError):
+            test_get = immutable_dict['b']
+
+    def test_contains(self):
+        immutable_dict = immutabledict({ 'a': 'value' })
+        assert 'a' in immutable_dict
+
+    def test_contains_not_existing(self):
+        immutable_dict = immutabledict({ 'a': 'value' })
+        assert 'b' not in immutable_dict
