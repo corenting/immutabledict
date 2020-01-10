800970784fefa6d3fc9bc16c13efd813a55b6f05
Corentin Garcia <cgarcia@meilleursagents.com>
2020-01-10 16:52:03 +0100

    tests: add tests

--- tests/test_immutabledict.py
+++ tests/test_immutabledict.py
@@ -30,5 +30,47 @@ class TestImmutableDict:
         assert 'a' in immutable_dict
 
     def test_contains_not_existing(self):
-        immutable_dict = immutabledict({ 'a': 'value' })
-        assert 'b' not in immutable_dict
+        immutable_dict = immutabledict({"a": "value"})
+        assert "b" not in immutable_dict
+
+    def test_copy(self):
+        original = immutabledict({"a": "value"})
+        copy = original.copy()
+        assert original == copy
+
+    def test_iter(self):
+        immutable_dict = immutabledict({"a": "value", "b": "other_value"})
+        itered_keys = {x for x in immutable_dict}
+        assert immutable_dict.keys() == itered_keys
+
+    def test_len(self):
+        immutable_dict = immutabledict({"a": "value", "b": "other_value"})
+        assert len(immutable_dict) == 2
+
+    def test_len_empty(self):
+        immutable_dict = immutabledict({})
+        assert len(immutable_dict) == 0
+
+    def test_repr(self):
+        immutable_dict = immutabledict({"a": "value", "b": "other_value"})
+        repr_ret = repr(immutable_dict)
+        assert repr_ret.startswith('<immutabledict')
+        assert repr_ret.endswith('>')
+
+    def test_hash(self):
+        first_dict = immutabledict({"a": "value", "b": "other_value"})
+        second_dict = immutabledict({"a": "value", "b": "other_value"})
+
+        assert hash(first_dict) == hash(second_dict)
+
+class ImmutableOrderedDict:
+
+    def test_ordered(self):
+        ordered = ImmutableOrderedDict()
+        ordered['a'] = '1'
+        ordered['b'] = '2'
+        ordered['c'] = '3'
+        itered_keys = {x for x in immutable_dict}
+        assert itered_keys[0] == 'a'
+        assert itered_keys[1] == 'b'
+        assert itered_keys[2] == 'c'
